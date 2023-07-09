508dd54ee026bbbbdfa25d68bd9129e454f80cd7
oangsa <sukruangkul.aongsa@gmail.com>
2023-07-09 23:47:09 +0700

    fix cron function

--- app/api/cron/resetMonth/route.ts
+++ app/api/cron/resetMonth/route.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { NextResponse } from 'next/server'
 import prisma from '@/libs/prismadb'
 
-export default async function GET( req: NextApiRequest, res: NextApiResponse ) {
+export async function GET( req: NextApiRequest, res: NextApiResponse ) {
     try {
 
         const res = await prisma.count.findFirst({})

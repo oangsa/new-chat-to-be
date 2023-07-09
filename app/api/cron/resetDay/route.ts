30b17f6145114f4059c333853928af87b753b75a
oangsa <sukruangkul.aongsa@gmail.com>
2023-07-09 23:52:13 +0700

    route

--- app/api/cron/resetDay/route.ts
+++ app/api/cron/resetDay/route.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { NextResponse } from 'next/server'
 import prisma from '@/libs/prismadb'
 
-export async function GET( req: NextApiRequest, res: NextApiResponse ) {
+export async function GET( ) {
     try {
 
         const res = await prisma.count.findFirst({})

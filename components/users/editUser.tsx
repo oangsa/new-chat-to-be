784f142f04bc0800cde8b318289be1dc3d3b0c94
oangsa <sukruangkul.aongsa@gmail.com>
2023-07-11 04:33:43 +0000

    fix

--- components/users/editUser.tsx
+++ components/users/editUser.tsx
@@ -101,7 +101,7 @@ function EditUser(props: props) {
             <IconButton onClick={handler}>
                 <EditIcon size={20} fill="#979797" />
             </IconButton>
-            <Modal closeButton aria-labelledby="modal-title" open={visible} onClose={closeHandler} >
+            <Modal closeButton aria-labelledby="edit-modal" open={visible} onClose={closeHandler} >
                 <Modal.Body>
                     <div className="flex justify-between items-center mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
